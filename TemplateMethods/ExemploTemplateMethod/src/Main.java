dbbe1500e51be99a846dd37f6d06516ce40f3f32
Alexandre Boer <ale_boer@Outlook.com>
2020-09-21 19:40:21 -0300

    Código exemplificando o uso do Template Method
    Código simulando um video player para exemplificar o uso do Template Method.

---
+++ TemplateMethods/ExemploTemplateMethod/src/Main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			VideoPlayer videoPlayer = null;
+			System.out.print("Digite o nome do arquivo: ");
+			String filename = reader.readLine();
+			
+			if (filename.indexOf("mkv") > 0) {
+			    videoPlayer = new MkvPlayer(filename);
+			} else if (filename.indexOf("mp4") > 0) {
+				videoPlayer = new Mp4Player(filename);
+			} else {
+				throw new IllegalArgumentException(filename + "type not supported");
+			}
+			
+			videoPlayer.playVideo();
+	}
+}
